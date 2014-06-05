acccef7e72e867e5c3f484f230604342d492b8b8
Mike Helbig <helbig.mike@gmail.com>
2014-06-04 22:31:31 -0500

    Began implementing new menu structure. Created new tabs for each menu section

--- MenuStructure.ino
+++ MenuStructure.ino
@@ -10,250 +10,60 @@
 //Root menu
 MenuSystem ms;
 Menu mm("    PanTron 4000");
+
 //Timelapse menu
-Menu tlMenu("Timelapse Video");
-MenuItem videoTime("Set video time");
-MenuItem shootTime("Set shoot time");
+Menu tlMenu                 ("Timelapse Video");
+MenuItem transitionToSet    ("Transition to set");
+MenuItem setPositions       ("Set positions");
+MenuItem setVideoTime       ("Set video time");
+MenuItem addDelTransition   ("Add/Del transition");
+MenuItem setShootTime       ("Set shoot time");
+MenuItem setStartDelay      ("Set start delay");
+MenuItem systemReady        ("System ready");
+MenuItem countdownToStart   ("Countdown to start");
+MenuItem runningSequence    ("Running sequence");
+
 //panoramic menu
-Menu pmMenu("360 Panograph");
-MenuItem pm_autoMode("Panograph Auto");
+Menu pmMenu                 ("360 Panograph");
+MenuItem pm_autoMode        ("Panograph Auto");
+
 //Setup menu
-Menu suMenu("Change Settings");
-MenuItem framesPerSecond("Frames/sec");
-MenuItem motionProfile("Motion Profile");
-MenuItem Xmotion("X (Pan) Motion");
-MenuItem Ymotion("Y (Tilt) Motion");
-MenuItem Zmotion("Z (Zoom) Motion");
-MenuItem Dmotion("D (Dolly) Motion");
-MenuItem Rmotion("R (Rotate) Motion");
-MenuItem FreeMem("Free Memory");
+Menu suMenu                 ("Change Settings");
+MenuItem framesPerSecond    ("Frames/sec");
+MenuItem motionProfile      ("Motion Profile");
+MenuItem FreeMem            ("Free Memory");
 
 /////////////////////////////////////////////////////////////////////////////////
 // Initialize
 /////////////////////////////////////////////////////////////////////////////////
 void initializeMenu(void)
 {
-  //Timelapse menu
+//Timelapse menu
   mm.add_menu(&tlMenu);
-  tlMenu.add_item(&videoTime, &on_videoTime_selected);
-  tlMenu.add_item(&shootTime, &on_shootTime_selected);
-  //Panoramic menu
+//  tlMenu.add_item(&transitionToSet,  &on_transitionToSet_selected);
+//  tlMenu.add_item(&setPositions,     &on_setPositions_selected);
+  tlMenu.add_item(&setVideoTime,     &on_set_videoTime_selected);
+//  tlMenu.add_item(&addDelTransition, &on_addDelTransition_selected;
+  tlMenu.add_item(&setShootTime,     &on_set_shootTime_selected);
+//  tlMenu.add_item(&setStartDelay,    &on_setStartDelay_selected);
+//  tlMenu.add_item(&systemReady,      &on_systemReady_selected);
+//  tlMenu.add_item(&countdownToStart, &on_coundownToStart_selected);
+//  tlMenu.add_item(&runningSequence,  &on_runningSequence_selected);
+
+//Panoramic menu
   mm.add_menu(&pmMenu);
   pmMenu.add_item(&pm_autoMode, &on_pm_autoMode_selected);
-  //Setup menu
+
+//Setup menu
   mm.add_menu(&suMenu);
   suMenu.add_item(&framesPerSecond, &on_framesPerSecond_selected);
   suMenu.add_item(&motionProfile, &on_motionProfile_selected);
-  suMenu.add_item(&Xmotion, &on_Xmotion_selected);
-  suMenu.add_item(&Ymotion, &on_Ymotion_selected);
-  suMenu.add_item(&Zmotion, &on_Zmotion_selected);
-  suMenu.add_item(&Dmotion, &on_Dmotion_selected);
-  suMenu.add_item(&Rmotion, &on_Rmotion_selected);
   suMenu.add_item(&FreeMem, &on_FreeMem_selected);
-  //Root menu (always last)  
+
+//Root menu (always last)  
   ms.set_root_menu(&mm);
 
   Serial.println("Menu system initialized");
   displayMenu();
 }
-
-/////////////////////////////////////////////////////////////////////////////////
-// Callback functions
-/////////////////////////////////////////////////////////////////////////////////
-void on_videoTime_selected(MenuItem* p_menu_item)
-{
-  static float tempVideoTimeSetting;
-  
-  // callback function "constructor"
-  if (ms.menu_item_was_just_selected())
-  {
-    lcd.clear();
-    displaySetHeading("Set video time");
-    displayNumericSetInstructions();
-    
-    tempVideoTimeSetting = (float)videoTimeSetting;  // save a copy of the current setting in case we cancel
-  }
-
-  // callback function main:
-  if( adjustNumericValue(&tempVideoTimeSetting,5,1800,true) )  // 5 seconds to 30 minutes
-    displayLongAsDDHHMMSS(round(tempVideoTimeSetting));
-
-  // callback function "destructor"
-  if(nunchuk.userInput == 'C' || nunchuk.userInput == 'Z')
-  {
-    ms.deselect_set_menu_item();
-    displayMenu();
-
-    if(nunchuk.userInput == 'Z')  // if Z is pressed we keep the newly adjusted value
-    {
-      videoTimeSetting = round(tempVideoTimeSetting);
-    }
-  }
-}
-
-void on_shootTime_selected(MenuItem* p_menu_item)
-{
-  static float tempShootTimeSetting;
-  
-  // callback function "constructor"
-  if (ms.menu_item_was_just_selected())
-  {
-    lcd.clear();
-    displaySetHeading("Set shoot time");
-    displayNumericSetInstructions();
-    
-    tempShootTimeSetting = (float)shootTimeSetting;  // save a copy of the current setting in case we cancel
-  }
-
-  // callback function main:
-  if( adjustNumericValue(&tempShootTimeSetting,300,2592000,true) )  //5 minutes to 30 days
-    displayLongAsDDHHMMSS(round(tempShootTimeSetting));
-
-  // callback function "destructor"
-  if(nunchuk.userInput == 'C' || nunchuk.userInput == 'Z')
-  {
-    ms.deselect_set_menu_item();
-    displayMenu();
-
-    if(nunchuk.userInput == 'Z')  // if Z is pressed we keep the newly adjusted value
-    {
-      shootTimeSetting = round(tempShootTimeSetting);
-    }
-  }
-}
-
-void on_pm_autoMode_selected(MenuItem* p_menu_item)
-{
-  Serial.print("pma ");
-  lcd.setCursor(0,1);
-  lcd.print("Pano Mode = AUTO");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  displayMenu();
-}
-
-void on_framesPerSecond_selected(MenuItem* p_menu_item)
-{
-  Serial.print("fps ");
-  lcd.setCursor(0,1);
-  lcd.print("Frames/sec = 30");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  displayMenu();
-}
-
-void on_motionProfile_selected(MenuItem* p_menu_item)
-{
-  static int enumIndex = 2;
-  int tempEnumIndex;
-  
-  enumeratedMenuList motionProfileList[]=
-  {
-    { "Step  "   ,0},  // corresponds to the spline.cpp setDegree() function
-    { "Linear" ,1},
-    { "Spline" ,11}  // we use the catmull spline type because it's easer to setup
-  };
-  
-  // callback function "constructor"
-  if (ms.menu_item_was_just_selected())
-  {
-    lcd.clear();
-    displaySetHeading("Set motion profile");
-
-    lcd.setCursor(0,1);
-    lcd.print(motionProfileList[enumIndex].menuText);
-    tempEnumIndex = enumIndex;
-  }
-
-  // callback function main:
-  if(selectEnumeratedValue(&tempEnumIndex,3))
-    {
-      lcd.setCursor(0,1);
-      lcd.print(motionProfileList[tempEnumIndex].menuText);
-    }
-
-  // callback function "destructor"
-  if(nunchuk.userInput == 'C' || nunchuk.userInput == 'Z')
-  {
-    ms.deselect_set_menu_item();
-    displayMenu();
-
-    if(nunchuk.userInput == 'Z')  // if Z is pressed we keep the newly adjusted value
-    {
-      enumIndex = tempEnumIndex;
-      selectedMotionProfile = motionProfileList[enumIndex].value;
-    }
-  }
-}
-
-void on_Xmotion_selected(MenuItem* p_menu_item)
-{
-  //  Serial.print("Inside the on_Xmotion menu, ");
-  lcd.setCursor(0,1);
-  lcd.print("Enabled");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  waitForCbutton();
-}
-
-void on_Ymotion_selected(MenuItem* p_menu_item)
-{
-  //  Serial.print("Inside the on_Ymotion menu, ");
-  lcd.setCursor(0,1);
-  lcd.print("Enabled");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  waitForCbutton();
-}
-
-void on_Zmotion_selected(MenuItem* p_menu_item)
-{
-  //  Serial.print("Inside the on_Zmotion menu, ");
-  lcd.setCursor(0,1);
-  lcd.print("disabled");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  waitForCbutton();
-}
-
-void on_Dmotion_selected(MenuItem* p_menu_item)
-{
-  //  Serial.print("Inside the on_Dmotion menu, ");
-  lcd.setCursor(0,1);
-  lcd.print("disabled");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  waitForCbutton();
-}
-
-void on_Rmotion_selected(MenuItem* p_menu_item)
-{
-  //  Serial.print("Inside the on_Rmotion menu, ");
-  lcd.setCursor(0,1);
-  lcd.print("Disabled");
-  if(nunchuk.userInput == 'C') ms.deselect_set_menu_item();
-  //  waitForCbutton();
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-// FREE MEMORY Display
-///////////////////////////////////////////////////////////////////////////////////////////////////
-void on_FreeMem_selected(MenuItem* p_menu_item)
-{
-  // callback function "constructor"
-  if (ms.menu_item_was_just_selected())
-  {
-    lcd.clear();
-    lcd.setCursor(0,1);
-    lcd.print("Free Memory =");
-    lcd.setCursor(14,1);
-    lcd.print(freeMemory(),DEC); 
-    lcd.setCursor(0,3);
-    lcd.print("Press Z to Exit"); 
-  }
-
-  // callback function main:
-
-  // callback function "destructor"
-  if(nunchuk.userInput == 'C')
-  {
-    ms.deselect_set_menu_item();
-    displayMenu();
-  }
-}
-
 

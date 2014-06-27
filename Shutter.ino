fd1c858a659c1f6ad19433d824cf15cf89f8f8c4
Mike Helbig <helbig.mike@gmail.com>
2014-06-26 21:27:04 -0500

    Added shutter tab

---
+++ Shutter.ino
@@ -0,0 +1,22 @@
+#define FOCUS_PIN           A3
+#define SHUTTER_PIN         A4
+
+void pressShutterButton(void)
+{
+  digitalWrite(SHUTTER_PIN,HIGH);
+}
+
+void releaseShutterButton(void)
+{
+  digitalWrite(SHUTTER_PIN,LOW);
+}
+
+void initializeShutterControl(void)
+{
+  digitalWrite(FOCUS_PIN,LOW);
+  digitalWrite(SHUTTER_PIN,LOW);
+
+  pinMode(FOCUS_PIN, OUTPUT);
+  pinMode(SHUTTER_PIN, OUTPUT);
+}
+
